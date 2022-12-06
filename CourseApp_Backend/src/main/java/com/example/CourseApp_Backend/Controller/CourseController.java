17956b26e2f671d1a739075643f70a627576c9b6
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-06 11:14:06 +0530

    add commit

--- CourseApp_Backend/src/main/java/com/example/CourseApp_Backend/Controller/CourseController.java
+++ CourseApp_Backend/src/main/java/com/example/CourseApp_Backend/Controller/CourseController.java
@@ -1,7 +1,15 @@
 package com.example.CourseApp_Backend.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CourseController {
+
+    @GetMapping("/add")
+    public String Addpage(){
+        return "Welcome to course add page";
+    }
+
+
 }
