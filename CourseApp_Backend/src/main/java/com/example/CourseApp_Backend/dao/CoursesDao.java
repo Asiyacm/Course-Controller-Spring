6f97d3fe1e2981b89f88eadc5d3f587b96da9409
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-07 17:32:08 +0530

    search api  commit

--- CourseApp_Backend/src/main/java/com/example/CourseApp_Backend/dao/CoursesDao.java
+++ CourseApp_Backend/src/main/java/com/example/CourseApp_Backend/dao/CoursesDao.java
@@ -1,7 +1,14 @@
 package com.example.CourseApp_Backend.dao;
 
 import com.example.CourseApp_Backend.Model.Courses;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CoursesDao extends CrudRepository<Courses,Integer> {
+
+    @Query(value = "SELECT `id`, `date`, `description`, `duration`, `title`, `venue` FROM `courses` WHERE `title` LIKE %:title%",nativeQuery = true)
+    List<Courses>Search(@Param("title") String title);
 }
